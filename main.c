eb6e60e73be775bf67ecfdc4370a79ccb46c1c05
Nícolas de Souza Moreira <d2022014472@unifei.edu.br>
2024-09-29 16:01:24 -0300

    Adds functions for matrix manipulation.

--- main.c
+++ main.c
@@ -1,36 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "matrizFunc.c"
+
+
 int main() {
-    FILE *file;
+    // matrizA = Matriz dos pixel RGB
+    // matrizB = Matriz de filtro
+    int ***matrizA;
+    int ***matrizB;
+    int ***matResult;
+
+    // n = Tamanho da matriz, em todos os casos são  de matriz quadradas então será n^2
+    char format[3]; // P3
+    int n;
+    int colorMax;
+    int rgb = 3;
+
     char filepath[100];
-    int largura, altura, maxCor;
-    char formato[3];
 
-    
+    // Garante que não ocorra estouro de buffer
     snprintf(filepath, sizeof(filepath), "../test cases/1.in");
-    file = fopen(filepath, "r");
-
+    FILE *file = fopen(filepath, "r");
 
     if (file == NULL) {
         perror("Erro ao abrir o arquivo");
-        return EXIT_FAILURE;
+        return 1;
     }
 
-    // Leitura do cabeçalho da imagem PPM (formato, largura, altura e escala de cor)
-    fscanf(file, "%s", formato);
-    if (strcmp(formato, "P3") != 0) {
-        printf("Formato de arquivo não suportado.\n");
-        fclose(file);
-        return EXIT_FAILURE;
-    }
+    fscanf(file, "%s", format);
+    fscanf(file, "%d %d", &n, &n);
+    fscanf(file, "%d", &colorMax);
 
-    fscanf(file, "%d %d", &largura, &altura);
-    fscanf(file, "%d", &maxCor);
+    printf("%s\n", format);
+    printf("%d %d\n", n, n);
+    printf("%d", colorMax);
 
-    printf("Formato: %s\n", formato);
-    printf("Largura: %d, Altura: %d, Max. Cor: %d\n", largura, altura, maxCor);
+    // Manipulação das matrizes
+
+    matrizA = alocaMatriz(n, rgb);
+    leMatriz(file, matrizA, n, rgb);
+    imprimeMatriz(matrizA, n, rgb);
+
+    matrizB   = alocaMatriz(n, rgb);
+    leMatriz(file, matrizB, n, rgb);
+    imprimeMatriz(matrizA, n, rgb);
+
+
+
+
+
+
     fclose(file);
-
-
 }
---
+++ matrizFunc.c
@@ -0,0 +1,40 @@
+
+int ***alocaMatriz(int n, int rgb) {
+    int ***matriz = malloc(sizeof(int**) * n);
+
+    for (int i = 0; i < n; i++) {
+        matriz[i] = malloc(sizeof(int*) * rgb);
+        for (int j = 0; j < rgb; j++)
+            matriz[i][j] = malloc(sizeof(int) * n);
+    }
+
+    return matriz;
+}
+
+void liberaMatriz(int ***matriz, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            free(matriz[i][j]);
+    }
+    free(matriz);
+}
+
+void leMatriz(FILE *file, int ***matriz, int n, int rgb) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < rgb; k++) {
+                fscanf(file, "%d", &matriz[i][j][k]);
+            }
+        }
+    }
+}
+
+void imprimeMatriz(int ***matriz, int n, int rgb) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < rgb; k++)
+                printf("%d ", matriz[i][j][k]);
+        }
+    }
+}
